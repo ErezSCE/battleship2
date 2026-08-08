393021c0e8be709b951c1bec0afced82412b5909
ErezSCE <samisoftware12@gmail.com>
2026-08-08 13:06:23 +0300

    [battleship2]-[GATE-python-test]-chore: final cleanup for battleship2/fix/gate-python-test-python-test (#10)
    Co-authored-by: ErezKon <erezkon@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
-import sys
 import os
+import pathlib
+import sys
+
 # Add the src directory to sys.path so that imports like 'src.app.main' work
-src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(src_path)
+src_path = pathlib.Path(__file__).resolve().parent.parent
+sys.path.append(str(src_path))
